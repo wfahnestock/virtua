f719405f6b810de05a7fd6ac202de00d985283f3
Will Fahnestock <wfahnestock@access411.com>
2025-07-21 09:29:58 -0400

    Remove interop code - Just use mstsc.exe for Remote Desktop sessions instead

--- composeApp/src/desktopMain/kotlin/org/access411/rdpclient/interop/NetworkCredential.kt
+++
@@ -1,8 +0,0 @@
-package org.access411.rdpclient.interop
-
-data class NetworkCredential(
-    val domain: String = "",
-    val password: String,
-    val securePassword: String? = null,
-    val userName: String,
-)
